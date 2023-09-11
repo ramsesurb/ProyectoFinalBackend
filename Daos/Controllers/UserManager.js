1bc9489c87448206ed410a8a9dda523c8757f028
Ramses  Urbina <ramsesurb@gmail.com>
2023-09-11 15:47:24 -0300

    Update UserManager.js

--- Daos/Controllers/UserManager.js
+++ Daos/Controllers/UserManager.js
@@ -1,19 +1,30 @@
-
+import usermodel from "./dao/controllers/userManager.js"
 
 class UserManager {
   
   async getUser() {
     try {
-      
-      const getUser = {user: req.session.user}
+      const getUser = { user: req.session.user }
       console.log("usuario", getUser);
       return getUser;
     } catch (error) {
       console.log(error);
     }
   }
+
+  async updateUserToPremium(userId) {
+    try {
+      const updatedUser = await usermodel.findOneAndUpdate(
+        { _id: userId },
+        { $set: { rol: 'premium' } },
+        { new: true }
+      );
+
+      return updatedUser;
+    } catch (error) {
+      console.log(error);
+    }
   }
+}
 
-
-export default UserManager
-const rute = new UserManager();
+export default UserManager;
